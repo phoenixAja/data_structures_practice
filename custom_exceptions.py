2a48c697c38ca4a5de1f8b8aaf17670599e88330
Phoenix <plogan@celgene.com>
2017-11-05 20:52:00 -0800

    directory cleaning

--- custom_exceptions.py
+++ custom_exceptions.py
@@ -4,3 +4,9 @@ class EmptyItemsError(Exception):
     Raised when items is empty
 	used for peek() and get() variations
     """
+
+class WrongTypeException(Exception):
+	"""
+	Raised when wrong type is encountered for data
+	structure implementation
+	"""
